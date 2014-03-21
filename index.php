6545d027771daaf051b4d6f22157d573c0db6b69
EpicTeemu <teemu.merilainen@gmail.com>
2014-03-21 15:05:55 +0200

    require header fixed

--- index.php
+++ index.php
@@ -15,4 +15,4 @@ define('WP_USE_THEMES', true);
 
 /** Loads the WordPress Environment and Template */
 /** OLD: require( dirname( __FILE__ ) . '/wp-blog-header.php' ); */
-require('./wordpress/wp-blog-header.php');
+require( dirname( __FILE__ ) . '/wordpress/wp-blog-header.php');
